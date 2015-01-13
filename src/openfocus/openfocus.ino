98223f282d909cd378e9f5ab57052e43e6cfb607
Jonathan Lurie <jonathanlurie@pc50.home>
2015-01-13 22:22:37 +0100

    edt

--- src/openfocus/openfocus.ino
+++ src/openfocus/openfocus.ino
@@ -11,7 +11,7 @@ but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 Lesser General Public License for more details.
 You should have received a copy of the GNU Lesser General Public
-License along with this library.
+License along with this library. 
 */
 
 
@@ -24,15 +24,15 @@ const int BASIC_DELAY = 3;
 // if under this value, we consider the joystick as static
 const float STICK_THRESH = 0.02;
 
- 
+
 // JOYSTICK
 const int _verticalPin = 5;  // analog
 const int _horizontalPin = 6;  // analog
 const int _selectorPin = 2;  // digital, allow Interrupt
 long _chronoThresh; // default is 500ms, but can be set
 Joystick *_focusStick;
- 
- 
+
+
 // STEPPER
 // the pin for direction
 const int _motorDirPin = 4;
@@ -52,24 +52,24 @@ int _stepsToMove = 0;
 // LED
 const int _ledPin = 13;
 long _chronoLedStart;
- 
+
 
 void setup() {
-  
+
   // SERIAL
   Serial.begin(57600);
-  
+
   // JOYSTICK
   _focusStick = new Joystick(_selectorPin, _horizontalPin, _verticalPin);
   _chronoThresh = _focusStick->getPressTimeThreshold();
-  
+
   // joystick pushbutton is mapped on interruption
   attachInterrupt(0, manageChrono, CHANGE);
-  
+
   // STEPPER
   _motor.setTotalNbSteps(_totalNbStep360);
   _motor.setRPM(_speedRP);
-  
+
   // LED
   pinMode(_ledPin, OUTPUT);
   digitalWrite(_ledPin, HIGH);
@@ -77,56 +77,56 @@ void setup() {
 
 void loop() {
 
-  
+
   // reading the focus joystick value
   float focusStickNormValue = _focusStick->getNormalizedVertical();
-  
-  
+
+
   // TODO
   // reading influence potentiometer value
   float influence = 1.0;
-  
+
   // TODO
   // reading the switch to revert rotation
   boolean revertRotation = false;
-  
-  
+
+
   // the stick is pushed or pulled
   if(abs(focusStickNormValue ) > STICK_THRESH){
-  
-    // XOR between stick rotation direction and the switch that reverts  
+
+    // XOR between stick rotation direction and the switch that reverts
     // TODO : doesnt work when revert is true!!
     if(!(focusStickNormValue > 0) != !revertRotation){
       _motor.setDirectionClockWise();
-      
+
       // in case of reccording
       _stepsToMove ++;
     }else{
       _motor.setDirectionCounterClockWise();
-      
+
       // in case of reccording
       _stepsToMove --;
     }
-    
+
           Serial.println(_stepsToMove);
-    
+
     // final delay between twoo movings
     long movingDelay = (float)BASIC_DELAY * 1000.0 * influence * (1.0 / abs(focusStickNormValue) );
-    
+
     //Serial.println(movingDelay);
-    
+
     _motor.renegateQuarterStep();
    // _motor.renegateStep();
-    
+
     if(movingDelay > 2000){
       delay((int)(movingDelay/1000.));
     }else{
       delayMicroseconds(movingDelay);
     }
-    
-    
+
+
   }else{
-    
+
     // the led might be switched off because of a long press on the joystick
     // if longer than, it must be switched on
     if((millis() - _chronoLedStart) > _chronoThresh && _chronoLedStart != 0){
@@ -137,68 +137,66 @@ void loop() {
       digitalWrite(_ledPin, HIGH);
       _chronoLedStart = 0;
     }
-    
+
     // the stick is idle, just wait...
     delay(50);
   }
-  
+
 }
 
 
 // deals with the joystick push button and go back to saved position
 void manageChrono(){
-  
+
   if(_focusStick->isPressed()){  // PRESS event
     // blink the led
     digitalWrite(_ledPin, LOW);
     _chronoLedStart = millis();
-  
+
     Serial.println("start chrono");
     _focusStick->startReccordTime();
-    
+
   }else{  // RELEASE event
     // unblink the led
     digitalWrite(_ledPin, HIGH);
-  
+
     Serial.println("stop chrono");
     boolean isLongPressed = _focusStick->stopReccordTime();
     Serial.println(isLongPressed);
-    
-    
+
+
     if(isLongPressed){ // LONG PRESS
       // reinitialize the position to reccord this one
       _stepsToMove  = 0;
-    
+
     }else{  // SHORT PRESS
       // we cancel the confirmation led blink
       _chronoLedStart = 0;
-    
+
       // does not move if does not need
       if(_stepsToMove == 0){
         return;
       }
-      
+
       Serial.println(_stepsToMove);
-      
+
       // go to the position previously reccorded
-      
+
       // choose the direction (not the shortest, but the real one!)
       if(_stepsToMove < 0){
         _motor.setDirectionClockWise();
       }else{
         _motor.setDirectionCounterClockWise();
       }
-      
+
       // performs the rotations
       _motor.rotationSteps(abs(_stepsToMove/4));
-      
+
       // reset the counter
       _stepsToMove = 0;
-      
+
     }
-    
+
   }
-  
+
 }
-
-
